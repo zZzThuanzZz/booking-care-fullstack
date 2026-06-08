dbd76e1227eb7b6b50dd7aac8a8e56cea0ce3b8e
zZzThuanzZz <kid.thuan,nguyen@gmail.com>
2026-06-08 10:16:19 +0700

    Node.js backend setup with Express and EJS

---
+++ Nodejs/src/config/viewEngine.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+let configViewEngine = (app) => {
+    app.use(express.static('../public'));
+    app.set('view engine', 'ejs')
+    app.set('views', '../views')
+}
+
+module.exports = configViewEngine;
