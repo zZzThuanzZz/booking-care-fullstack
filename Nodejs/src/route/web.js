ef3f711b5322ec268c30b2071dad91e7e402a069
zZzThuanzZz <kid.thuan,nguyen@gmail.com>
2026-06-15 18:29:56 +0700

    connect to MySQL database using Sequelize

--- Nodejs/src/route/web.js
+++ Nodejs/src/route/web.js
@@ -4,10 +4,7 @@ import homeController from '../controllers/homeController';
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get('/', (req, res) => {
-        return res.send('hello world');
-    });
-    router.get('/thuan', homeController.getHomePage);
+    router.get('/', homeController.getHomePage);
     router.get('/about', homeController.getAboutPage);
     return app.use('/', router)
 }
