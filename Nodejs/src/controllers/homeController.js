a60cfa3462593333c1d5f3d877de2465978fd8ad
zZzThuanzZz <kid.thuan,nguyen@gmail.com>
2026-07-26 11:43:30 +0700

    create new data using Sequelize ORM

--- Nodejs/src/controllers/homeController.js
+++ Nodejs/src/controllers/homeController.js
@@ -1,4 +1,6 @@
 import db from "../models/index"
+import CRUDSevices from "../services/CRUDSevices";
+
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
@@ -13,7 +15,19 @@ let getHomePage = async (req, res) => {
 let getAboutPage = (req, res) => {
     return res.render('./test/about.ejs')
 }
+
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs')
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDSevices.createNewUser(req.body)
+    console.log(message)
+    return res.send('post CRUD from server')
+}
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD
 }
