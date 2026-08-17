321c7db6531b95cb0b2104160b0185ad46d9280a
zZzThuanzZz <kid.thuan,nguyen@gmail.com>
2026-08-17 16:11:32 +0700

    display data using Sequelize ORM

--- Nodejs/src/controllers/homeController.js
+++ Nodejs/src/controllers/homeController.js
@@ -25,9 +25,18 @@ let postCRUD = async (req, res) => {
     console.log(message)
     return res.send('post CRUD from server')
 }
+
+let displayGetCRUD = async (req, res) => {
+    let data = await CRUDSevices.getAllUser();
+    console.log('----------------------------------')
+    console.log(data)
+    console.log('----------------------------------')
+    return res.render('displayCRUD.ejs', { dataTable: data })
+}
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
     getCRUD: getCRUD,
-    postCRUD: postCRUD
+    postCRUD: postCRUD,
+    displayGetCRUD: displayGetCRUD
 }
--- Nodejs/src/services/CRUDSevices.js
+++ Nodejs/src/services/CRUDSevices.js
@@ -37,6 +37,20 @@ let hashUserPassword = (password) => {
         }
     })
 }
+
+let getAllUser = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let users = db.User.findAll({
+                raw: true
+            });
+            resolve(users)
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
 module.exports = {
-    createNewUser: createNewUser
+    createNewUser: createNewUser,
+    getAllUser: getAllUser
 }
