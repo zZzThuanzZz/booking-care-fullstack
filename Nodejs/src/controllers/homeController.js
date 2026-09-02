d53665febc82af3dfa5aecb16b6d7f86f93d690d
zZzThuanzZz <kid.thuan,nguyen@gmail.com>
2026-09-02 20:21:03 +0700

    edit data using Sequelize ORM

--- Nodejs/src/controllers/homeController.js
+++ Nodejs/src/controllers/homeController.js
@@ -31,12 +31,41 @@ let displayGetCRUD = async (req, res) => {
     console.log('----------------------------------')
     console.log(data)
     console.log('----------------------------------')
-    return res.render('displayCRUD.ejs', { dataTable: data })
+    return res.render('displayCRUD.ejs', {
+        dataTable: data
+    })
 }
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDSevices.getUserInforById(userId);
+        //check user data not found
+
+        // let userData
+        return res.render('editCRUD.ejs', {
+            user: userData
+        })
+    }
+    else {
+        return res.send('Users not found!')
+    }
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDSevices.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers
+    })
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
-    displayGetCRUD: displayGetCRUD
+    displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD
 }
--- Nodejs/src/services/CRUDSevices.js
+++ Nodejs/src/services/CRUDSevices.js
@@ -50,7 +50,55 @@ let getAllUser = () => {
         }
     })
 }
+
+let getUserInforById = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: userId },
+                raw: true
+            })
+            if (user) {
+                resolve(user)
+            }
+            else {
+                resolve({})
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+let updateUserData = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: data.id }
+            })
+            if (user) {
+                user.firstName = data.firstName;
+                user.lastName = data.lastName;
+                user.address = data.address;
+                user.phonenumber = data.phonenumber;
+
+                await user.save();
+                let allUsers = await db.User.findAll();
+                resolve(allUsers);
+            }
+            else {
+                resolve();
+            }
+
+        } catch (e) {
+            console.log(e)
+        }
+    })
+}
+// #27 41:19
 module.exports = {
     createNewUser: createNewUser,
-    getAllUser: getAllUser
+    getAllUser: getAllUser,
+    getUserInforById: getUserInforById,
+    updateUserData: updateUserData
 }
